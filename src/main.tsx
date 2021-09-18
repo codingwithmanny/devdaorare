70f8efb756624dbef97fe433d4992882e7855bac
codingwithmanny <codingwithmanny@gmail.com>
2021-09-18 16:05:20 -0400

    Aded abi file, utils, removed images

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,12 @@
+// Imports
+// ========================================================
 import React from 'react'
 import ReactDOM from 'react-dom'
 import './index.css'
 import App from './App'
 
+// Main Render
+// ========================================================
 ReactDOM.render(
   <React.StrictMode>
     <App />
---
+++ src/utils/constants.ts
@@ -0,0 +1,5 @@
+/**
+ * Main contract address Devs for Revolution
+ * https://etherscan.io/address/0x25ed58c027921e14d86380ea2646e3a1b5c55a8b
+ */
+export const CONTRACT_ADDRESS = '0x25ed58c027921e14d86380ea2646e3a1b5c55a8b';
