cf4a7dd8e98276dd8b28719b48c571dadabe5b10
codingwithmanny <codingwithmanny@gmail.com>
2021-09-19 10:28:20 -0400

    Added base layout, more data json, and fonts

---
+++ src/providers/chakra.tsx
@@ -0,0 +1,20 @@
+// Imports
+// ========================================================
+import React from 'react'
+import { ChakraProvider } from '@chakra-ui/react'
+import theme from '../theme'
+
+// Chakra Provider
+// ========================================================
+/**
+ *
+ * @param param0
+ * @returns
+ */
+const ChakraProviderWrapper: React.FC = ({ children }) => {
+  return <ChakraProvider theme={theme}>{children}</ChakraProvider>
+}
+
+// Exports
+// ========================================================
+export default ChakraProviderWrapper
